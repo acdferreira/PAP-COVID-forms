c12a05bda7123504d79229a51a6104a8134236cc
acdferreira <76686918+acdferreira@users.noreply.github.com>
2020-12-28 16:40:04 +0000

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,14 @@
+var config = {
+  // email da unidade para onde as pessoas possam comunicar
+  email: "maildaunidade@servio.pt",
+  form1: {
+    // URL do flow que lê e processa os dados do form1 - Formulário dos Casos Positivos
+    url: "https://prod-109.westeurope.logic.azure.com:443/workflows/8ffab01ef4fb4f6f91dbe780fe2c94fd/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ldbOkJk-nK7sourExlAVXE0811lAd1HYnBw-hxMrSWo"
+  },
+  form2: {
+    url: ""
+  },
+  form3: {
+    url: ""
+  }
+}
