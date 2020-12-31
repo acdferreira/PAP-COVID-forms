46a8c7359a5afd7b16139502f67bbed7bed4d6c5
acdferreira <76686918+acdferreira@users.noreply.github.com>
2020-12-31 11:24:01 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ var config = {
     url: "https://prod-109.westeurope.logic.azure.com:443/workflows/8ffab01ef4fb4f6f91dbe780fe2c94fd/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ldbOkJk-nK7sourExlAVXE0811lAd1HYnBw-hxMrSWo"
   },
   form2: {
-    url: ""
+    url: "https://prod-189.westeurope.logic.azure.com:443/workflows/7ae4736ffaeb43c0b7ea9bbfbc1f52a3/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=YrukeKGe6UtQQaQfyq1QY4z3-17llLNPKbAWBJOGhas"
   },
   form3: {
     url: ""
